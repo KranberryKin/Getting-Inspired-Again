a8813d0d2ce7a2ca313d9cf89dd330da6b77045e
Kranberrykin <quinnclimbs@gmail.com>
2025-07-29 14:33:25 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import BrowserRouter from "react-router-dom"
+import {Router as BrowserRouter} from "react-router-dom"
 
 const App = () => {
   return (
