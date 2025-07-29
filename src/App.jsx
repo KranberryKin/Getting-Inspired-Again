1946b3bb16bf69dc7b70189585e4b9c6b2f8b528
Kranberrykin <quinnclimbs@gmail.com>
2025-07-29 14:31:39 -0500

    Update Github Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import BrowserRouter from "react-router-dom"
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <BrowserRouter basename="/Getting-Inspired-Again" />
       </header>
     </div>
   );
